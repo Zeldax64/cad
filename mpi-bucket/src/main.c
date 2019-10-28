86c76c9330a5b9023e6fd06df63329c99b8a3569
Zelda <caioxyz@hotmail.com>
2019-10-28 16:08:39 -0300

    Add MPI to mpi-bucket

--- mpi-bucket/src/main.c
+++ mpi-bucket/src/main.c
@@ -1,30 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#include <mpi.h>
 
 #define THREADS 4
-#define NUM_KEYS 80
-#define MAX_KEY 30
+#define NUM_KEYS 48
+#define MAX_KEY 5
 
-void rank(int* key_array);
+void rank(int* key_array, const int SIZE);
 void rand_key(int* key_array, int size);
 void print_keys(int* keys, int size);
+void parallel_buckets(int size, int *key_array, int *buckets);
+void unbucket(int *keys, int *buckets);
 
 int main () {
 	int size = NUM_KEYS;
+	int *keys = NULL;
+	int *gather_keys = NULL;
+	int *buckets = NULL;
+	int *reduced_buckets = NULL;
+	int world_rank, world_size, elements_per_proc;
+
+	MPI_Init(NULL, NULL);
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+	if(world_rank == 0) {
+		keys = (int*)malloc(size*sizeof(int));
+		gather_keys = (int*)malloc(size*sizeof(int)); 
+		reduced_buckets = (int*)malloc(MAX_KEY*sizeof(int)); 
+		rand_key(keys, size);
+		printf("Input: ");		
+		print_keys(keys, size);
+		
+	}
+	buckets = (int*)malloc(MAX_KEY*sizeof(int)); 
 	
-	int *keys;
-	keys = (int*) malloc(size*sizeof(int));
-	rand_key(keys, THREADS*NUM_KEYS);
+	elements_per_proc = NUM_KEYS/world_size;
+	int *sub_keys = (int*)malloc(elements_per_proc*sizeof(int));
+	MPI_Scatter(keys, 
+				elements_per_proc,
+				MPI_INT,
+				sub_keys,
+				elements_per_proc,
+				MPI_INT,
+				0,
+				MPI_COMM_WORLD);
 
-	print_keys(keys, size); // Print iput array.
-	rank(keys);
-	print_keys(keys,size); // Print output array.
+/*
+	MPI_Gather(sub_keys,
+			   elements_per_proc,
+			   MPI_INT,
+			   gather_keys,
+			   elements_per_proc,
+			   MPI_INT,
+			   0,
+			   MPI_COMM_WORLD);
+	
+	if(world_rank == 0) {
+		resort_gather_keys(keys, gather_keys, size);
+		printf("Output: ");
+		print_keys(keys, size);
+	}
+*/
 
-	free(keys);
+	parallel_buckets(elements_per_proc, sub_keys, buckets);
+	
+	printf("Rank %d: ", world_rank);
+	print_keys(buckets,MAX_KEY);
+
+	MPI_Reduce(
+		buckets,
+		reduced_buckets,
+		MAX_KEY,
+		MPI_INT,
+		MPI_SUM,
+		0,
+		MPI_COMM_WORLD
+		);
+
+
+	if(world_rank == 0) {
+		print_keys(reduced_buckets, MAX_KEY);
+		unbucket(keys, reduced_buckets);
+		printf("Unbucket: ");
+		print_keys(keys, size);
+		free(keys);
+	}
+	free(sub_keys);
+
+	MPI_Finalize();
 }
 
-void rank(int* key_array) {
+void rank(int* key_array, const int SIZE) {
 	int i, k;
 	int *key_buff_ptr;
 
@@ -52,7 +120,7 @@ void rank(int* key_array) {
 		}
 
 		#pragma omp for nowait
-		for( i=0; i<NUM_KEYS; i++ ) {
+		for( i=0; i<SIZE; i++ ) {
 			work_buff[key_array[i]]++;
 		}
 
@@ -61,7 +129,7 @@ void rank(int* key_array) {
 
 		#pragma omp barrier
 		for( myid=1; myid<num_procs; myid++ ) {
-			#pragma omp for nowait
+			#pragma omp for //nowait
 			for( i=0; i<MAX_KEY; i++ )
 				key_buff_ptr[i] += key_buff1[myid][i];
 		}
@@ -73,6 +141,7 @@ void rank(int* key_array) {
 				key_array[i++] = k;
 		}
 	} /*omp parallel*/
+
 }
 
 // Helper functions.
@@ -87,4 +156,69 @@ void print_keys(int* keys, int size) {
 		printf("%d ", keys[i]);
 	}
 	printf("\n");
+}
+
+void parallel_buckets(int size, int *key_array, int *buckets) {
+	int i;
+	int *key_buff_ptr;
+
+	int** key_buff1;
+
+	key_buff1 = (int**)malloc(THREADS*sizeof(int*));
+	for(int i = 0; i < THREADS; ++i) {
+		key_buff1[i] = (int*)calloc(MAX_KEY, sizeof(int));
+	}
+	key_buff_ptr = key_buff1[0];
+
+	#pragma omp parallel private(i)
+	{
+		int *work_buff;
+		int myid = 0, num_procs = 1;
+
+		myid = omp_get_thread_num();
+		num_procs = omp_get_num_threads();
+
+		work_buff = key_buff1[myid];
+
+		for( i=0; i<MAX_KEY; i++ ) {
+			work_buff[i] = 0;
+		}
+
+		#pragma omp for nowait
+		for( i=0; i<size; i++ ) {
+			work_buff[key_array[i]]++;
+		}
+
+		for( i=0; i<MAX_KEY-1; i++ ) 
+			work_buff[i+1] += work_buff[i];
+
+		#pragma omp barrier
+		for( myid=1; myid<num_procs; myid++ ) {
+			#pragma omp for //nowait
+			for( i=0; i<MAX_KEY; i++ )
+				key_buff_ptr[i] += key_buff1[myid][i];
+		}
+	} /*omp parallel*/
+
+	// Copy return.
+	for(i = 0; i < MAX_KEY; ++i) {
+		buckets[i] = key_buff_ptr[i];
+	}
+
+	// Free data.
+	for(int i = 0; i < THREADS; ++i) {
+		free(key_buff1[i]);
+	}
+	free(key_buff1);
+}
+
+void unbucket(int *keys, int *buckets) {
+	int i, k;
+
+	#pragma omp parallel for private(i, k)
+	for(k = 0; k < MAX_KEY; k++) {
+		i = (k==0)? 0 : buckets[k-1];
+		while (i<buckets[k])
+			keys[i++] = k;
+	}
 }
